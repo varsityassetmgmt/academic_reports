a51f2397cb1cd1fc83fe3392e8fbdba31af6eb10
MohanKrishna-varsity <mohankrishna.c@varsitymgmt.com>
2025-10-04 14:37:11 +0530

    upto exam instance

--- AcademicReports/branches/urls.py
+++ AcademicReports/branches/urls.py
@@ -8,6 +8,12 @@ router.register(r'state_dropdown', StateDropdownViewSet, basename='state_dropdow
 router.register(r'zone_dropdown', ZoneDropdownViewSet, basename='zone_dropdown')
 router.register(r'branch_dropdown', BranchDropdownViewSet, basename='branch_dropdown')
 
+router.register(r'state_dropdown_for_exam', StateDropdownForExamViewSet, basename='state_dropdown_for_exam')
+router.register(r'zone_dropdown_for_exam', ZoneDropdownForExamViewSet, basename='zone_dropdown_for_exam')
+router.register(r'branch_dropdown_for_exam', BranchDropdownForExamViewSet, basename='branch_dropdown_for_exam')
+
+
+
 urlpatterns = [
 
     
