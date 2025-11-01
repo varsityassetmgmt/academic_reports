ecc7f456f25d48d33668591ad529b7c04c3a7aff
MohanKrishna-varsity <mohankrishna.c@varsitymgmt.com>
2025-11-01 11:57:36 +0530

    Update signals.py

--- AcademicReports/exams/signals.py
+++ AcademicReports/exams/signals.py
@@ -432,7 +432,6 @@ def handle_exam_result_post_save(sender, instance, created, **kwargs):
 def handle_exam_skill_result_post_save(sender, instance, created, **kwargs):
     exam = instance.exam_result.exam_instance.exam
     student = instance.exam_result.student
-    print(instance.pk)
 
     # Async skill grade update
     tasks.update_exam_skill_result_grade.delay(instance.pk)
